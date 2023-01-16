fc7f74b7a8dd9079f47b29a06f6cc0859d9abc70
Moshe Krumbein <moshekrum@gmail.com>
2023-01-16 22:20:17 +0200

    Day 14, 2015: Less cloning

--- aoc2015/src/day/day13.rs
+++ aoc2015/src/day/day13.rs
@@ -4,24 +4,22 @@ use itertools::Itertools;
 
 use super::{Day, Input, Types};
 
-type Pairs = HashMap<(String, String), i32>;
-type People = HashSet<String>;
+type Pairs<'a> = HashMap<(&'a str, &'a str), i32>;
+type People<'a> = HashSet<&'a str>;
 
 fn process(input: &str) -> (Pairs, People) {
     let mut matchings: Pairs = HashMap::new();
     let mut people: People = HashSet::new();
     for line in input.lines() {
         let split: Vec<&str> = line.split_ascii_whitespace().collect();
-        let (person1, person2) = (
-            split[0].to_string(),
-            split[10][..split[10].len() - 1].to_string(),
-        );
+        let person1 = split[0];
+        let person2 = &split[10][..split[10].len() - 1];
         let happiness: i32 = match *split.get(2).unwrap() {
             "gain" => split[3].parse::<i32>().unwrap(),
             "lose" => -split[3].parse::<i32>().unwrap(),
             _ => panic!("Invalid happiness value"),
         };
-        matchings.insert((person1.clone(), person2.clone()), happiness);
+        matchings.insert((person1, person2), happiness);
         people.insert(person1);
         people.insert(person2);
     }
@@ -33,10 +31,10 @@ fn max_happiness(matchings: &Pairs, people: &People) -> i32 {
     for permutation in people.iter().permutations(people.len()) {
         let mut happiness = 0;
         for i in 0..permutation.len() {
-            let person1 = &permutation[i].clone();
-            let person2 = &permutation[(i + 1) % permutation.len()].clone();
-            happiness += matchings[&(person1.clone(), person2.clone())];
-            happiness += matchings[&(person2.clone(), person1.clone())];
+            let person1 = *permutation[i];
+            let person2 = *permutation[(i + 1) % permutation.len()];
+            happiness += matchings[&(person1, person2)];
+            happiness += matchings[&(person2, person1)];
         }
         if happiness > max_happiness {
             max_happiness = happiness;
@@ -51,10 +49,10 @@ pub fn part_a(input: &Input) -> Types {
 }
 pub fn part_b(input: &Input) -> Types {
     let (mut matchings, mut people) = process(&input.contents);
-    people.insert("Me".to_string());
+    people.insert("Me");
     for person in &people {
-        matchings.insert(("Me".to_string(), person.clone()), 0);
-        matchings.insert((person.clone(), "Me".to_string()), 0);
+        matchings.insert(("Me", person), 0);
+        matchings.insert((person, "Me"), 0);
     }
     let max_happiness = max_happiness(&matchings, &people);
     Types::Number(max_happiness.try_into().unwrap())
