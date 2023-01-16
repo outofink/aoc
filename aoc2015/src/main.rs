5df8f0eb452928838f90e1ca359125b372dfd551
Moshe Krumbein <moshekrum@gmail.com>
2023-01-16 11:27:43 +0200

    2015: Improved error checking on program input

--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -1,6 +1,10 @@
 mod day;
 use std::env;
 fn main() {
-    let day = env::args().nth(1).unwrap().parse().unwrap();
+    let day = env::args()
+        .nth(1)
+        .expect("No day number passed")
+        .parse::<u8>()
+        .expect("Day number must be a number");
     day::run(day);
 }
